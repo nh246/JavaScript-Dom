86fd097dd182bc03d5c6b178692eea7d10c6d9e3
nh246 <nazmul210130@diit.edu.bd>
2024-02-21 17:45:25 +0600

    copy bg

---
+++ Copy-code/script.js
@@ -0,0 +1,34 @@
+window.onload = () =>{
+    main();
+
+}
+
+function main(){
+    const root = document.getElementById('root')
+    const btn = document.getElementById('btn')
+    const hex = document.getElementById('hexvalue')
+    const copybtn = document.getElementById('copy_btn')
+
+    copybtn.addEventListener('click',function(){
+        navigator.clipboard.writeText(hex.value)
+    })
+    
+
+    btn.addEventListener('click', function(){
+        const bgColor = generateColors()
+        root.style.backgroundColor = bgColor;
+        hex.value = bgColor
+    })
+
+}
+
+function generateColors() {
+    const red = Math.floor(Math.random()*255);
+    const green = Math.floor(Math.random()*255);
+    const blue = Math.floor(Math.random()*255);
+
+    return (`#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`)
+
+
+}
+
